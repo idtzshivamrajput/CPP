1e06376fc7b9a2569ede874dfc65a705b86f77c6
idtzshivamrajput <113540823+idtzshivamrajput@users.noreply.github.com>
2022-10-06 18:41:01 +0530

    gg

--- tut6.cpp
+++ tut6.cpp
@@ -1,9 +1,20 @@
 #include<iostream>  // header file hai ye, comes with compiler
-
+                    //    header file is of two type      
+                    //   -System header file
+                    //   - user defined header file
 
 using namespace std;
 
 int main(){
-    cout<<"hemlo world";
+    int a=4, b=5;
+    cout<<"Operators in c++;";
+    cout<<"/n types of operators in c++ are -";
+    // Arithmatic Operator
+    cout<<"the value of a + b is"<<a+b;
+    cout<<"the value of a - b is"<<a-b;
+    cout<<"the value of a * b is"<<a+b;
+    cout<<"the value of a + b is"<<a+b;
+    cout<<"the value of a + b is"<<a+b;
+
     return 0;
 }
